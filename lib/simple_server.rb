b95de1f869e5aae3e158b198ba2a43f2f24d8561
Jacek Wargol <jwargol@gmail.com>
2017-01-03 20:48:39 +0100

    Parse request with POST method

--- lib/simple_server.rb
+++ lib/simple_server.rb
@@ -1,13 +1,27 @@
 require 'socket'
+require 'json'
 
 server = TCPServer.open(2000)
 loop do
   Thread.start(server.accept) do |client|
-    request = client.gets
-    path = request.split[1]
 
-    if request.split[0] == 'GET'
+    headers = ''
+    while (line = client.gets)
+      headers << line
+      break if headers =~ /\r\n\r\n$/
+    end
 
+    method = headers.split[0]
+    path = headers.split[1]
+
+    body_length = headers[/content-length:\s+[0-9]+/i].split(/\s+/)[1].to_i
+    body = ''
+    while (char = client.getc) do
+      body << char
+      break if body.length >= body_length
+    end
+
+    if method == 'GET'
       # TODO: add proper headings
       if File.exists?("..#{path}")
         file = File.open("..#{path}")
@@ -17,6 +31,20 @@ loop do
         response = %{HTTP/1.0 404 Not Found\r\n\r\n}
       end
       client.puts(response)
+
+    elsif method == 'POST'
+      if File.exists?("..#{path}")
+        puts 'post exists'
+        file = File.open("..#{path}")
+        headers, body = request.split("\r\n\r\n", 2)
+        puts bpdy
+        params = JSON.parse(body)
+        puts "asa #{body}"
+        response = %{HTTP/1.0 200 OK\r\n\r
+        #{file.read}}
+      else
+        response = %{HTTP/1.0 404 Not Found\r\n\r\n}
+      end
     end
     client.close
   end
--- lib/tiny_web_browser.rb
+++ lib/tiny_web_browser.rb
@@ -1,15 +1,32 @@
 require 'socket'
 require 'net/http'
+require 'json'
 
 
 host = 'localhost'
 port = 2000
-path = '/index.html'
-
-request = "GET #{path} HTTP/1.0\r\n\r\n"
-
 socket = TCPSocket.open(host, port)
-socket.print(request)
+path = '/thanks.html'
+method = 'POST'
+
+if method == 'POST'
+  # puts 'What is your name?'
+  # name = gets.chomp
+  # puts 'What is pur email address?'
+  # email = gets.chomp
+  name = 'a'
+  email = 'em'
+  user = {user: {name: name, email: email}}
+  # TODO: add proper headings
+  request = %{#{method} #{path} HTTP/1.0\r
+Content-Length: #{user.to_json.length}\r\n\r
+#{user.to_json}}
+else
+  request = "#{method} #{path} HTTP/1.0\r\n\r\n"
+end
+
+puts request
+socket.puts(request)
 response = socket.read
 headers, body = response.split("\r\n\r\n", 2)
 status_code = headers.split[1]
